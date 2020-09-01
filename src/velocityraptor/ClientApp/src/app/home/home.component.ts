63a234a6550d7aa57c80d8f0d17143bb26125b09
Fabio Salvalai <fabio@seesharp.ch>
2020-09-01 14:46:51 +0200

    develop start front-end

--- src/velocityraptor/ClientApp/src/app/home/home.component.ts
+++ src/velocityraptor/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public products: Product;
+
+  constructor (private client: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    // TODO: move to dedicated service
+    this.client.get<Product>('/api/products').subscribe(o => this.products = o);
+  }
+}
+
+// todo: move to dedicated file
+interface Product {
+  id: string;
+  name: string;
+  developers: any[];
+  sprints: any[];
 }
